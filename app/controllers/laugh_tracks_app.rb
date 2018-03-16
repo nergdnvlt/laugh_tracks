c3438e77bb9c83c1f050fb9da9209d2046ac615a
nergdvnlt <tylerlundgren@yahoo.com>
2018-03-16 10:17:45 -0600

    Building out initial functionality.

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,2 +1,6 @@
+# Controller to manage traffic through routes.
 class LaughTracksApp < Sinatra::Base
+  get '/comedians' do
+    erb :"comedians/index", :locals => { :all_comedians => Comedian.all }
+  end
 end
